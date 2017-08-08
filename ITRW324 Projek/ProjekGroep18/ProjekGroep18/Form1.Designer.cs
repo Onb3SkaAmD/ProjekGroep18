76624fa4d9bea489487be5cdb69bde0148123c67
Onb3SkaAmD <30688716+Onb3SkaAmD@users.noreply.github.com>
2017-08-08 11:46:16 +0200

    Add files via upload
    First upload

---
+++ ITRW324 Projek/ProjekGroep18/ProjekGroep18/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ProjekGroep18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRuanSwardt = new System.Windows.Forms.Button();
+            this.btnRuanvanZyl = new System.Windows.Forms.Button();
+            this.btnStefan = new System.Windows.Forms.Button();
+            this.btnCharl = new System.Windows.Forms.Button();
+            this.btnJean = new System.Windows.Forms.Button();
+            this.btnHenko = new System.Windows.Forms.Button();
+            this.btnTiaan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRuanSwardt
+            // 
+            this.btnRuanSwardt.Location = new System.Drawing.Point(13, 13);
+            this.btnRuanSwardt.Name = "btnRuanSwardt";
+            this.btnRuanSwardt.Size = new System.Drawing.Size(80, 23);
+            this.btnRuanSwardt.TabIndex = 0;
+            this.btnRuanSwardt.Text = "Ruan Swardt";
+            this.btnRuanSwardt.UseVisualStyleBackColor = true;
+            // 
+            // btnRuanvanZyl
+            // 
+            this.btnRuanvanZyl.Location = new System.Drawing.Point(99, 12);
+            this.btnRuanvanZyl.Name = "btnRuanvanZyl";
+            this.btnRuanvanZyl.Size = new System.Drawing.Size(87, 23);
+            this.btnRuanvanZyl.TabIndex = 1;
+            this.btnRuanvanZyl.Text = "Ruan van Zyl";
+            this.btnRuanvanZyl.UseVisualStyleBackColor = true;
+            // 
+            // btnStefan
+            // 
+            this.btnStefan.Location = new System.Drawing.Point(192, 12);
+            this.btnStefan.Name = "btnStefan";
+            this.btnStefan.Size = new System.Drawing.Size(75, 23);
+            this.btnStefan.TabIndex = 2;
+            this.btnStefan.Text = "Stefan";
+            this.btnStefan.UseVisualStyleBackColor = true;
+            // 
+            // btnCharl
+            // 
+            this.btnCharl.Location = new System.Drawing.Point(18, 42);
+            this.btnCharl.Name = "btnCharl";
+            this.btnCharl.Size = new System.Drawing.Size(75, 23);
+            this.btnCharl.TabIndex = 3;
+            this.btnCharl.Text = "Charl";
+            this.btnCharl.UseVisualStyleBackColor = true;
+            // 
+            // btnJean
+            // 
+            this.btnJean.Location = new System.Drawing.Point(99, 42);
+            this.btnJean.Name = "btnJean";
+            this.btnJean.Size = new System.Drawing.Size(75, 23);
+            this.btnJean.TabIndex = 4;
+            this.btnJean.Text = "Jean";
+            this.btnJean.UseVisualStyleBackColor = true;
+            // 
+            // btnHenko
+            // 
+            this.btnHenko.Location = new System.Drawing.Point(180, 42);
+            this.btnHenko.Name = "btnHenko";
+            this.btnHenko.Size = new System.Drawing.Size(75, 23);
+            this.btnHenko.TabIndex = 5;
+            this.btnHenko.Text = "Henko";
+            this.btnHenko.UseVisualStyleBackColor = true;
+            // 
+            // btnTiaan
+            // 
+            this.btnTiaan.Location = new System.Drawing.Point(18, 71);
+            this.btnTiaan.Name = "btnTiaan";
+            this.btnTiaan.Size = new System.Drawing.Size(75, 23);
+            this.btnTiaan.TabIndex = 6;
+            this.btnTiaan.Text = "Tiaan";
+            this.btnTiaan.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 272);
+            this.Controls.Add(this.btnTiaan);
+            this.Controls.Add(this.btnHenko);
+            this.Controls.Add(this.btnJean);
+            this.Controls.Add(this.btnCharl);
+            this.Controls.Add(this.btnStefan);
+            this.Controls.Add(this.btnRuanvanZyl);
+            this.Controls.Add(this.btnRuanSwardt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRuanSwardt;
+        private System.Windows.Forms.Button btnRuanvanZyl;
+        private System.Windows.Forms.Button btnStefan;
+        private System.Windows.Forms.Button btnCharl;
+        private System.Windows.Forms.Button btnJean;
+        private System.Windows.Forms.Button btnHenko;
+        private System.Windows.Forms.Button btnTiaan;
+    }
+}
+
